36d65436fa3a41fba2f9d9e8e152ba7cd15852cc
Dalvs <lopesribeirodalvana20@gmail.com>
2025-10-02 12:48:03 -0300

    add

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// Componente simples de cabeçalho
+export default function Header() {
+  return (
+    <header style={{ textAlign: "center", marginBottom: "20px" }}>
+      <h1>🌌 NASA - Foto do Dia</h1>
+      <p>Escolha uma data e veja a imagem do universo!</p>
+    </header>
+  );
+}
